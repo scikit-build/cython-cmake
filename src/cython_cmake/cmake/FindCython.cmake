e1528e8d6c0e98aa9a7325d88ef4c8fc7715bd1d
Omar Padron <omar.padron@kitware.com>
2016-06-30 16:23:29 -0500

    add experimental CMake Module: Cython
    Cherry-picked from scikit-build/scikit-build@3bb2ca7 ("add experimental CMake
    Module: Cython", 2016-06-30)

--- src/cython_cmake/cmake/FindCython.cmake
+++ src/cython_cmake/cmake/FindCython.cmake
@@ -1,11 +1,22 @@
-# Find the Cython compiler.
+#.rst:
+# FindCython
+# ----------
 #
-# This code sets the following variables:
+# Find ``cython`` executable.
 #
-#  CYTHON_EXECUTABLE
+# This module defines the following variables:
+#
+#  ``CYTHON_EXECUTABLE``
+#    path to the ``cython`` program
+#
+#  ``CYTHON_VERSION``
+#    version of ``cython``
+#
+#  ``CYTHON_FOUND``
+#    true if the program was found
 #
 # See also UseCython.cmake
-
+#
 #=============================================================================
 # Copyright 2011 Kitware, Inc.
 #
@@ -24,21 +35,43 @@
 
 # Use the Cython executable that lives next to the Python executable
 # if it is a local installation.
-find_package( PythonInterp )
-if( PYTHONINTERP_FOUND )
-  get_filename_component( _python_path ${PYTHON_EXECUTABLE} PATH )
-  find_program( CYTHON_EXECUTABLE
-    NAMES cython cython.bat cython3
-    HINTS ${_python_path}
-    )
+find_package(PythonInterp)
+if(PYTHONINTERP_FOUND)
+  get_filename_component(_python_path ${PYTHON_EXECUTABLE} PATH)
+  find_program(CYTHON_EXECUTABLE
+               NAMES cython cython.bat cython3
+               HINTS ${_python_path}
+               DOC "path to the cython executable")
 else()
-  find_program( CYTHON_EXECUTABLE
-    NAMES cython cython.bat cython3
-    )
+  find_program(CYTHON_EXECUTABLE
+               NAMES cython cython.bat cython3
+               DOC "path to the cython executable")
 endif()
 
+if(CYTHON_EXECUTABLE)
+  set(CYTHON_version_command ${CYTHON_EXECUTABLE} --version)
 
-include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( Cython REQUIRED_VARS CYTHON_EXECUTABLE )
+  execute_process(COMMAND ${CYTHON_version_command}
+                  OUTPUT_VARIABLE CYTHON_version_output
+                  ERROR_VARIABLE CYTHON_version_error
+                  RESULT_VARIABLE CYTHON_version_result
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-mark_as_advanced( CYTHON_EXECUTABLE )
+  if(NOT ${CYTHON_version_result} EQUAL 0)
+    set(_error_msg "Command \"${CYTHON_version_command}\" failed with")
+    set(_error_msg "${_error_msg} output:\n${CYTHON_version_error}")
+    message(SEND_ERROR "${_error_msg}")
+  else()
+    if("${CYTHON_version_output}" MATCHES "^[Cc]ython version ([^,]+)")
+      set(CYTHON_VERSION "${CMAKE_MATCH_1}")
+    endif()
+  endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Cython REQUIRED_VARS CYTHON_EXECUTABLE)
+
+mark_as_advanced(CYTHON_EXECUTABLE)
+
+include(UseCython)
+
