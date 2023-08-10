9ee51753e8ffd3984a4f8a8bde1d65fda08a5dac
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-10 13:23:48 -0400

    feat: first version
    Based of scientific-python/cookie@8ad07bc000e229287bd8e89dc4177a53183c00c1
    and generated running `cookiecutter gh:scientific-python/cookie`

---
+++ src/cython_cmake/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2024 Jean-Christophe Fillion-Robin. All rights reserved.
+
+cython-cmake: CMake helpers for building Cython modules
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
