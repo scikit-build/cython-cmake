c306737829f900f3ee8c737acc085b93aae2a087
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-07-16 23:54:07 -0400

    tests: Add test_scikit_build_core
    Co-authored-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,9 +1,32 @@
 from __future__ import annotations
 
 import importlib.metadata
+import zipfile
+from pathlib import Path
+
+from scikit_build_core.build import build_wheel
 
 import cython_cmake as m
+
+DIR = Path(__file__).parent.resolve()
 
 
 def test_version():
     assert importlib.metadata.version("cython_cmake") == m.__version__
+
+
+def test_scikit_build_core(monkeypatch, tmp_path):
+    monkeypatch.chdir(DIR / "packages/simple")
+    build_dir = tmp_path / "build"
+
+    wheel = build_wheel(
+        str(tmp_path), {"build-dir": str(build_dir), "wheel.license-files": []}
+    )
+
+    with zipfile.ZipFile(tmp_path / wheel) as f:
+        file_names = set(f.namelist())
+    assert len(file_names) == 4
+
+    build_files = {x.name for x in build_dir.iterdir()}
+    assert "simple.c.dep" in build_files
+    assert "simple.c" in build_files
