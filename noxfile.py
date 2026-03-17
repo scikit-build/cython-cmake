d06ded1b3c39ab6a5dd0e29fe1b1dfbf2af4acfd
Henry Schreiner <HenrySchreinerIII@gmail.com>
2026-03-17 10:54:28 -0400

    chore: remove unused docs infra (#70)
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,6 @@
 
 from __future__ import annotations
 
-import argparse
 from pathlib import Path
 
 import nox
@@ -47,67 +46,6 @@ def tests(session: nox.Session) -> None:
     """
     session.install("-e.", "--group=test")
     session.run("pytest", *session.posargs)
-
-
-@nox.session(reuse_venv=True, default=False)
-def docs(session: nox.Session) -> None:
-    """
-    Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links.
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--serve", action="store_true", help="Serve after building")
-    parser.add_argument(
-        "-b", dest="builder", default="html", help="Build target (default: html)"
-    )
-    args, posargs = parser.parse_known_args(session.posargs)
-
-    if args.builder != "html" and args.serve:
-        session.error("Must not specify non-HTML builder with --serve")
-
-    extra_installs = ["sphinx-autobuild"] if args.serve else []
-
-    session.install("-e.", "--group=docs", *extra_installs)
-    session.chdir("docs")
-
-    if args.builder == "linkcheck":
-        session.run(
-            "sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck", *posargs
-        )
-        return
-
-    shared_args = (
-        "-n",  # nitpicky mode
-        "-T",  # full tracebacks
-        f"-b={args.builder}",
-        ".",
-        f"_build/{args.builder}",
-        *posargs,
-    )
-
-    if args.serve:
-        session.run("sphinx-autobuild", *shared_args)
-    else:
-        session.run("sphinx-build", "--keep-going", *shared_args)
-
-
-@nox.session(default=False)
-def build_api_docs(session: nox.Session) -> None:
-    """
-    Build (regenerate) API docs.
-    """
-
-    session.install("sphinx")
-    session.chdir("docs")
-    session.run(
-        "sphinx-apidoc",
-        "-o",
-        "api/",
-        "--module-first",
-        "--no-toc",
-        "--force",
-        "../src/cython_cmake",
-    )
 
 
 @nox.session(default=False)
